ddfadb9b090c638e84ab9b856ac31e946b9f7642
cantorsdust <cantorsdust@gmail.com>
2016-03-22 02:03:19 -0500

    Updated to SMAPI 0.38 with manifest.json and config.json.  See new release.
    Signed-off-by: cantorsdust <cantorsdust@gmail.com>

---
+++ NPCLocations--SMAPI/1.1.0.0.SMAPI/readme.txt
@@ -0,0 +1,50 @@
+Adaptation of Kemenor's Storm mod, NPCLocations, to SMAPI.  Shows a menu listing current NPC locations when you press Z or LeftShoulder on your controller.
+Kemenor's code, adapted for SMAPI by cantorsdust
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+INSTALLATION
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+The zip contains one folder, NPCLocations, with three files NPCLocations.dll, config.json, and manifest.json. The folder must be placed in %appdata%\StardewValley\Mods.
+
+Thus, the total path for all 3 of the files required for this mod to function are:
+%appdata%\StardewValley\Mods\NPCLocations\NPCLocations.dll
+
+AND
+
+%appdata%\StardewValley\Mods\NPCLocations\config.json
+
+
+AND
+
+%appdata%\StardewValley\Mods\NPCLocations\manifest.json
+
+
+REQUIRES SMAPI 0.38+ to be installed!  Version 1.0.0.SMPAI is for SMAPI 0.37
+https://github.com/ClxS/SMAPI/releases
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+USAGE
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+Run StardewModdingAPI.exe in your main Stardew Valley folder. This will load the mods and then start the game.
+
+Please note that this game comes with a config.json file with nine options:
+
+"ButtonKey": "LeftShoulder",
+"KeyboardKey": "Z
+
+
+You may configure these keys by editing the values shown.  
+Keyboard keys should be capitalized.  
+Possible ButtonKey values are listed here in the Member Name column: https://msdn.microsoft.com/en-us/library/microsoft.xna.framework.input.buttons.aspx
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+CHANGELOG
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+1.1.0.0.SMAPI
+Updated mod for SMAPI 0.38, included mod manifest.
+
+1.0.0.0.SMAPI
+First SMAPI port
--- NPCLocations--SMAPI/NPCLocations/NPCLocations.cs
+++ NPCLocations--SMAPI/NPCLocations/NPCLocations.cs
@@ -17,26 +17,7 @@ namespace NPCLocations
 {
     public class NPCLocations : Mod
     {
-        public override string Name
-        {
-            get { return "NPCLocations"; }
-        }
-
-        public override string Authour
-        {
-            get { return "Kemenor's code, adapted for SMAPI by cantorsdust"; }
-        }
-
-        public override string Version
-        {
-            get { return "1.0.0.0.SMAPI"; }
-        }
-
-        public override string Description
-        {
-            get { return "Adaptation of Kemenor's Storm mod to SMAPI.  Shows a menu listing current NPC locations when you press Z"; }
-        }
-
+        public static ModConfig NPCLocationsConfig { get; private set; }
 
         public override void Entry(params object[] objects)
         {
@@ -47,75 +28,19 @@ namespace NPCLocations
 
 
         }
-
-        public string ButtonKey = "LeftShoulder";
-        public string KeyboardKey = "Z";
         public bool showNPC = false;
 
         void runConfig()
         {
-            
-            string ConfigPathAppData = Environment.ExpandEnvironmentVariables("%AppData%\\StardewValley\\Mods\\NPCLocations.ini");
-            string ConfigPathSVMods = "Mods\\NPCLocations.ini";
-            string DLLPathAppData = Environment.ExpandEnvironmentVariables("%AppData%\\StardewValley\\Mods\\NPCLocations.dll");
-            string DLLPathSVMods = "Mods\\NPCLocations.dll";
-            string path = null;
-            char[] delimiterChars = { '=' };
-            if (File.Exists(ConfigPathAppData))
-            {
-                Console.WriteLine("found INI in %appdata%");
-                path = ConfigPathAppData;
-            }
-            else if (File.Exists(ConfigPathSVMods))
-            {
-                Console.WriteLine("found INI in Stardew Valley-Mods");
-                path = ConfigPathSVMods;
-            }
-            else
-            {
-                Console.WriteLine("WARNING:  Could not find INI.  Writing new INI with default values next to DLL");
-                if (File.Exists(DLLPathAppData))
-                {
-                    File.AppendAllLines(ConfigPathAppData, new[] { "ButtonKey=LeftShoulder", "KeyboardKey=Z" });
-
-                }
-                else if (File.Exists(DLLPathSVMods))
-                {
-                    File.AppendAllLines(ConfigPathAppData, new[] { "ButtonKey=LeftShoulder", "KeyboardKey=Z" });
-                }
-
-            }
-
-            if (path != null)
-            {
-                List<string> fileData = File.ReadAllLines(path).ToList();
-                //each fileData[index] is a line
-                //we'll check to see what each line holds and parse that line's data
-                foreach (string line in fileData)
-                {
-                    //Console.WriteLine("Parsing INI line " + index.ToString("g"));
-                    string[] words = line.Split(delimiterChars);
-                    //make sure you've fixed old INIs
-                    if (words[0].Contains("ButtonKey"))
-                    {
-                        ButtonKey = words[1];
-                        Console.WriteLine("ButtonKey is " + ButtonKey);
-                    }
-                    else if (words[0].Contains("KeyboardKey"))
-                    {
-
-                        KeyboardKey = words[1];
-                        Console.WriteLine("KeyboardKey is " + KeyboardKey);
-                    }
-                }
-            }            
+            NPCLocationsConfig = new ModConfig();
+            NPCLocationsConfig = (ModConfig)Config.InitializeConfig(Config.GetBasePath(this), NPCLocationsConfig);
         }
 
 
         public void Events_KeyPressed(object sender, EventArgsKeyPressed e)
         {
             //Console.WriteLine("Key pressed: " + e.KeyPressed.ToString());
-            if (e.KeyPressed.ToString().Equals(KeyboardKey))
+            if (e.KeyPressed.ToString().Equals(NPCLocationsConfig.KeyboardKey))
             {
                 HandleMenu();
             }
@@ -124,7 +49,7 @@ namespace NPCLocations
         public void Events_ControllerPressed(object sender, EventArgsControllerButtonPressed e)
         {
             //Console.WriteLine("Controller button pressed: " + e.ButtonPressed.ToString());
-            if (e.ButtonPressed.ToString().Equals(ButtonKey))
+            if (e.ButtonPressed.ToString().Equals(NPCLocationsConfig.ButtonKey))
             {
                 HandleMenu();
             }
@@ -145,6 +70,19 @@ namespace NPCLocations
                 Game1.activeClickableMenu = null;
                 showNPC = false;
             }
+        }
+    }
+
+    public class ModConfig : Config
+    {
+        public string ButtonKey { get; set; }
+        public string KeyboardKey { get; set; }
+
+        public override Config GenerateBaseConfig(Config baseConfig)
+        {
+            ButtonKey = "LeftShoulder";
+            KeyboardKey = "Z";
+            return this;
         }
     }
 }
